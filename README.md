1cba20dc2c22dcd0f4b276c458c3b42a5428803d
Thomas Gingele <thomas@gingele.de>
2023-08-12 16:50:45 +0200

    Multiple bug fixes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # s2busb
-Convert bash/PowerShell/... scripts to RubberyDucky script. Supports FlipperZero commands.
+
+## Notice
+
+This script is tailored towards the Flipper Zero and only supports **Rubber Ducky script version 1.0**!
+
+## Usage
+
+The script takes an input file and writes the contents to an output file.
+Any commands that are specific to Rubber Ducky script (e.g. `SHIFT DELETE`, `PRINTSCREEN`) can be specified in the source script through a comment.
+
+```bash
+./s2busb.py -i bash_script.sh -o rubber_script.txt
+./s2busb.py --input bash_script.sh --output rubber_script.txt
+```
+
+## Additional Resource
+
+- [Rubber Ducky Scripting Language v1.0 - web.archive.org](https://web.archive.org/web/20220816200129/http://github.com/hak5darren/USB-Rubber-Ducky/wiki/Duckyscript)
+- [BadUSB Documentation - docs.flipper.net](https://docs.flipper.net/bad-usb)
--- s2busb.py
+++ s2busb.py
@@ -17,6 +17,7 @@ def main():
     args = parser.parse_args()
     input_file  = args.input
     output_file = args.output
+    output_dir  = os.path.dirname(output_file)
 
     command_regex = re.compile('^(' +\
     '((DEFAULT_?)?DELAY [0-9]+)|' +\
@@ -24,12 +25,13 @@ def main():
     '(ALTCHAR .*)|' +\
     '(ALTSTRING .*)|' +\
     '(ALTCODE .*)|' +\
+    '(STRING .*)|' +\
     '((ALT-)?GUI( .)?)|' +\
     '((CTRL-|ALT-|GUI-)?SHIFT( (DELETE|HOME|INSERT|PAGEUP|PAGEDOWN|WINDOWS|GUI|UPARROW|DOWNARROW|LEFTARROW|RIGHTARROW|TAB))?)|' +\
     '((CTRL-)?ALT( (END|ESC|ESCAPE|F[1-9][1-2]?|.|SPACE|TAB))?)|' +\
     '((CONTROL|CTRL)( (F[1-9][1-2]?|.|BREAK|PAUSE|ESCAPE|ESC))?)|' +\
     '((DOWN|UP|LEFT|RIGHT)(ARROW)?)|' +\
-    '(BREAK|PAUSE|CAPSLOCK|DELETE|END|ESC|ESCAPE|HOME|INSERT|NUMLOCK|PAGEUP|PAGEDOWN|PRINTSCREEN|SCROLLLOCK|SPACE|TAB|FN)|' +\
+    '(BREAK|PAUSE|CAPSLOCK|DELETE|END|ESC|ESCAPE|HOME|INSERT|NUMLOCK|PAGEUP|PAGEDOWN|PRINTSCREEN|SCROLLLOCK|SPACE|TAB|FN|ENTER)|' +\
     '(REPEAT [0-9]+)|' +\
     ')$')
 
@@ -37,7 +39,8 @@ def main():
         print('Unable to read input file. Exiting')
         exit(-1)
 
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    if output_dir:
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
     with open(input_file,  'r') as fr_input_file, open(output_file, 'w') as fw_output_file:
         while True:
@@ -45,8 +48,8 @@ def main():
             if not line:
                 break
 
-            if line.startswith('#'):
-                line = line[1:]
+            if line.lstrip().startswith('#'):
+                line = line.lstrip()[1:]
                 line = line.strip()
 
                 if command_regex.match(line):
@@ -58,6 +61,7 @@ def main():
                 continue
 
             if line.isspace():
+                fw_output_file.write('\n')
                 continue
 
             fw_output_file.write('STRING ' + line + 'ENTER\n')
